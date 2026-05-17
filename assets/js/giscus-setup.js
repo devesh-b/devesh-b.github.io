091923f67b1f0fa4bd08c0b89ea8e6b9d0e0ed77
devesh-b <devesh-b@users.noreply.github.com>
2026-05-17 16:00:15 +0000

    Deploying to gh-pages from @ devesh-b/devesh-b.github.io@d527315c604bbc765bf544c27333830b5319216b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "devesh-b/devesh-b.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
